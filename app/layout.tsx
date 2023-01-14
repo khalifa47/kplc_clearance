2ef1de2588bd71858078527c11d165502859b9c4
khalifa47 <khalifafumo5@gmail.com>
2023-01-14 13:04:52 +0300

    project init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,16 @@
-import './globals.css'
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
-  )
+  );
 }
