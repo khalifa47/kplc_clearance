be1f773352faf4402cccc3605f3f2420ba1b1d10
khalifa47 <khalifafumo5@gmail.com>
2023-02-16 11:28:00 +0300

    root layout set with nav and children

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,23 +1,19 @@
 import Nav from "@/app/components/Nav";
-import Test from "@/app/components/Test";
 import { getServerSession } from "next-auth";
 import Providers from "@/app/components/Providers";
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
-  // if (session == null) {
-  //   signIn();
-  // }
   const session = await getServerSession();
+
   return (
     <html>
       <head />
       <body>
         <div style={{ display: "flex" }}>
           <Providers session={session}>
-            <Test />
+            <Nav />
+            {children}
           </Providers>
-          {/* <Nav /> */}
-          {/* {children} */}
         </div>
       </body>
     </html>
