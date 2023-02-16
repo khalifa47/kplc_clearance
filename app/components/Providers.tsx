e9c8c8dd90dd69c276ce1dffa1ef5d16c39d7439
khalifa47 <khalifafumo5@gmail.com>
2023-02-16 11:29:40 +0300

    provider TODO: redirect unauth to login

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -2,6 +2,7 @@
 
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+// import { useRouter } from "next/navigation";
 
 const Providers = ({
   session,
@@ -10,6 +11,11 @@ const Providers = ({
   session: Session | null;
   children: React.ReactNode;
 }) => {
+  // const router = useRouter();
+  // if (!session) {
+  //   router.replace("/login");
+  //   return <h1>Unauthenticated. Redirecting login page...</h1>;
+  // }
   return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
