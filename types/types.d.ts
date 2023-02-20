d68cc923d73bd609e59893da1f43c77480d5cc94
khalifa47 <khalifafumo5@gmail.com>
2023-02-20 11:43:44 +0300

    departments status types done

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,8 +1,12 @@
 type DepartmentStatus = {
-  department: string;
-  status: Status;
-  certifier: string;
-  date: string;
+  clearanceId: number;
+  clearedBy: string | null;
+  department: { name: string };
+  departmentId: number;
+  id: number;
+  status: { name: Status };
+  statusId: number;
+  user: { firstName: string; lastName: string } | null;
 };
 
 type Status = "progress" | "approved" | "pending";
@@ -14,14 +18,5 @@ type Clearance = {
   statusId: number;
   updatedAt: string;
   userId: string;
-  DepartmentClearance: {
-    clearanceId: number;
-    clearedBy: string | null;
-    department: { name: string };
-    departmentId: number;
-    id: number;
-    status: { name: Status };
-    statusId: number;
-    user: { firstName: string; lastName: string } | null;
-  };
+  DepartmentClearance: DepartmentStatus[];
 };
