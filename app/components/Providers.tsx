729b3e0150259309af1d7efa77a58a8d55e620bc
khalifa47 <khalifafumo5@gmail.com>
2023-03-02 15:24:37 +0300

    deny inactive users access

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -14,6 +14,13 @@ const Providers = ({
     signIn();
     return <h1>Unauthenticated. Redirecting login page...</h1>;
   }
+  if (session.user.statusId === 2) {
+    return (
+      <h1>
+        {`Inactive User: ${session.user.firstName} ${session.user.lastName} with ID ${session.user.id} has been cleared`}
+      </h1>
+    );
+  }
   return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
