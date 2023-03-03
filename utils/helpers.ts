b146052e5a475b7ede878ec9390d8627edf7a041
khalifa47 <khalifafumo5@gmail.com>
2023-03-03 12:16:28 +0300

    removed comments

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,7 +1,6 @@
 export const capitalize = (sentence: string) => {
-  // Split the sentence into words
   const words = sentence.split(" ");
-  // Capitalize the first letter of each word and join the words back together
+
   const capitalizedWords = words.map((word) => {
     return word.charAt(0).toUpperCase() + word.slice(1);
   });
